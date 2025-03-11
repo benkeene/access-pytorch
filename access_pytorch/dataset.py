c242e6ca6cf6da901a7a08f1a8dcf78f04a972f9
Benjamin Keene <ben@Benjamins-MacBook-Pro.local>
2025-03-11 09:01:11 -0400

    MNIST basics

--- access_pytorch/dataset.py
+++ access_pytorch/dataset.py
@@ -4,10 +4,62 @@ import typer
 from loguru import logger
 from tqdm import tqdm
 
+from torchvision import datasets, transforms
+import torch
+
 from access_pytorch.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 app = typer.Typer()
 
+def mnist_loaders(train_kwargs, test_kwargs):
+    logger.info(f"Creating MNIST train and test loaders with {train_kwargs} and {test_kwargs}...")
+    train_data, test_data = pull_mnist()
+
+    train_loader = torch.utils.data.DataLoader(train_data, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(test_data, **test_kwargs)
+
+    logger.success("Train and test loaders created.")
+    return train_loader, test_loader
+
+def cifar10_loaders(train_kwargs, test_kwargs):
+    logger.info(f"Creating CIFAR10 train and test loaders with {train_kwargs} and {test_kwargs}...")
+    train_data, test_data = pull_cifar10()
+    
+    train_loader = torch.utils.data.DataLoader(train_data, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(test_data, **test_kwargs)
+
+    logger.success("Train and test loaders created.")
+    return train_loader, test_loader
+
+def pull_mnist():
+    logger.info("Pulling MNIST dataset...")
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    logger.info(f"Applying {transform}")
+
+    train_data = datasets.MNIST(PROCESSED_DATA_DIR, train=True, download=True, transform=transform)
+    test_data = datasets.MNIST(PROCESSED_DATA_DIR, train=False, transform=transform)
+
+    logger.success("MNIST dataset processed, split, and transformed.")
+    return train_data, test_data
+
+def pull_cifar10():
+    logger.info("Pulling CIFAR10 dataset...")
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    logger.info(f"Applying {transform}")
+
+    train_data = datasets.CIFAR10(PROCESSED_DATA_DIR, train=True, download=True, transform=transform)
+    test_data = datasets.CIFAR10(PROCESSED_DATA_DIR, train=False, transform=transform)
+
+    logger.success("CIFAR10 dataset processed, split, and transformed.")
+    return train_data, test_data
 
 @app.command()
 def main(
